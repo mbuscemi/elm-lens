a68bc957ca18b5d82e41ab0daf48e95ba111bcf3
Matthew Buscemi <matthew.buscemi@gmail.com>
2017-12-16 11:40:53 -0800

    covered gathering references from tuples

---
+++ elm/tests/ASTParsing/Expressions/TupledExpression.elm
@@ -0,0 +1,72 @@
+module ASTParsing.Expressions.TupledExpression exposing (canParseFromTupled)
+
+import Dict
+import ElmFile
+import Expect
+import Set
+import Test exposing (Test, describe, test)
+import Types.Expression
+import Types.Reference exposing (Reference)
+
+
+canParseFromTupled : Test
+canParseFromTupled =
+    describe "References in Tupled Expression" <|
+        let
+            elmFile =
+                ElmFile.fromString "TupledExpression.elm" elmFileText
+        in
+        [ test "has expected module name" <|
+            \_ ->
+                Expect.equal elmFile.moduleName [ "Simple" ]
+        , test "has expected imports" <|
+            \_ ->
+                Expect.equal elmFile.imports
+                    { direct = Dict.empty
+                    , aliases = Dict.empty
+                    }
+        , test "has expected top level expressions" <|
+            \_ ->
+                Expect.equal elmFile.topLevelExpressions
+                    { functions =
+                        Dict.empty
+                            |> Dict.insert "reference1" (Types.Expression.standardExpression 2)
+                            |> Dict.insert "reference2" (Types.Expression.standardExpression 6)
+                            |> Dict.insert "tupledExpression" (Types.Expression.standardExpression 10)
+                    , types = Dict.empty
+                    , typeAliases = Dict.empty
+                    }
+        , test "has expected exposings" <|
+            \_ ->
+                Expect.equal elmFile.exposings
+                    { functions =
+                        Set.empty
+                            |> Set.insert "tupledExpression"
+                    , types = Set.empty
+                    }
+        , test "has expected references" <|
+            \_ ->
+                Expect.equal elmFile.references
+                    { internal = [ Reference "reference2", Reference "reference1" ]
+                    , external = Dict.empty
+                    }
+        ]
+
+
+elmFileText : String
+elmFileText =
+    """module Simple exposing (tupledExpression)
+
+reference1 : String
+reference1 =
+    "a"
+
+reference2 : Int
+reference2 =
+    1
+
+tupledExpression : Int
+tupledExpression =
+    ( reference1, reference2 )
+
+"""
