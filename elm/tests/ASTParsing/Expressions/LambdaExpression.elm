21496f65f64689e52c843a23d183d352852c08f1
Matthew Buscemi <matthew.buscemi@gmail.com>
2017-12-16 12:56:00 -0800

    added coverage for lambda expressions

---
+++ elm/tests/ASTParsing/Expressions/LambdaExpression.elm
@@ -0,0 +1,67 @@
+module ASTParsing.Expressions.LambdaExpression exposing (canParseFromLambda)
+
+import Dict
+import ElmFile
+import Expect
+import Set
+import Test exposing (Test, describe, test)
+import Types.Expression
+import Types.Reference exposing (Reference)
+
+
+canParseFromLambda : Test
+canParseFromLambda =
+    describe "Reference in Lambda Expression" <|
+        let
+            elmFile =
+                ElmFile.fromString "LambdaExpression.elm" elmFileText
+        in
+        [ test "has expected module name" <|
+            \_ ->
+                Expect.equal elmFile.moduleName [ "Simple" ]
+        , test "has expected imports" <|
+            \_ ->
+                Expect.equal elmFile.imports
+                    { direct = Dict.empty
+                    , aliases = Dict.empty
+                    }
+        , test "has expected top level expressions" <|
+            \_ ->
+                Expect.equal elmFile.topLevelExpressions
+                    { functions =
+                        Dict.empty
+                            |> Dict.insert "reference" (Types.Expression.standardExpression 2)
+                            |> Dict.insert "lambdaExpression" (Types.Expression.standardExpression 6)
+                    , types = Dict.empty
+                    , typeAliases = Dict.empty
+                    }
+        , test "has expected exposings" <|
+            \_ ->
+                Expect.equal elmFile.exposings
+                    { functions =
+                        Set.empty
+                            |> Set.insert "lambdaExpression"
+                    , types = Set.empty
+                    }
+        , test "has expected references" <|
+            \_ ->
+                Expect.equal elmFile.references
+                    { internal = [ Reference "reference" ]
+                    , external = Dict.empty
+                    }
+        ]
+
+
+elmFileText : String
+elmFileText =
+    """module Simple exposing (lambdaExpression)
+
+reference : Int
+reference =
+    5
+
+lambdaExpression : Int
+lambdaExpression =
+    (\\_ -> reference) ()
+
+"""
