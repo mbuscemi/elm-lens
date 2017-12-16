4e25c027789d09d478df2c3b945038b4b994477b
Matthew Buscemi <matthew.buscemi@gmail.com>
2017-12-16 09:21:10 -0800

    added a simple test with the difference of exposing all at the top level

--- elm/tests/ASTParsing/Simple.elm
+++ elm/tests/ASTParsing/Simple.elm
@@ -1,4 +1,4 @@
-module ASTParsing.Simple exposing (canParseSimple)
+module ASTParsing.Simple exposing (canParseSimple, canParseSimpleExposingAll)
 
 import Dict
 import ElmFile
@@ -67,12 +67,92 @@ canParseSimple =
         ]
 
 
+canParseSimpleExposingAll : Test
+canParseSimpleExposingAll =
+    describe "Simple Elm File (Exposing All)" <|
+        let
+            elmFile =
+                ElmFile.fromString "Simple.elm" simpleExposingAll
+        in
+        [ test "has expected module name" <|
+            \_ ->
+                Expect.equal elmFile.moduleName [ "Simple" ]
+        , test "has expected imports" <|
+            \_ ->
+                Expect.equal elmFile.imports
+                    { direct = Dict.empty
+                    , aliases = Dict.empty
+                    }
+        , test "has expected top level expressions" <|
+            \_ ->
+                Expect.equal elmFile.topLevelExpressions
+                    { functions =
+                        Dict.empty
+                            |> Dict.insert "blarg" (Types.Expression.standardExpression 2)
+                            |> Dict.insert "frangle" (Types.Expression.standardExpression 6)
+                            |> Dict.insert "blargargle" (Types.Expression.standardExpression 10)
+                            |> Dict.insert "toStringCanMakeNumberText" (Types.Expression.standardExpression 14)
+                    , types =
+                        Dict.empty
+                    , typeAliases =
+                        Dict.empty
+                    }
+        , test "has expected exposings" <|
+            \_ ->
+                Expect.equal elmFile.exposings
+                    { functions =
+                        Set.empty
+                            |> Set.insert "blarg"
+                            |> Set.insert "frangle"
+                            |> Set.insert "blargargle"
+                            |> Set.insert "toStringCanMakeNumberText"
+                    , types =
+                        Set.empty
+                    }
+        , test "has expected references" <|
+            \_ ->
+                Expect.equal elmFile.references
+                    { internal =
+                        [ Reference "frangle"
+                        , Reference "blarg"
+                        , Reference "blargargle"
+                        , Reference "toString"
+                        ]
+                    , external =
+                        Dict.empty
+                    }
+        ]
+
+
 simpleDotElm : String
 simpleDotElm =
     """module Simple exposing (blarg, frangle)
 
 import Dict exposing (empty)
 import Set exposing (insert)
+
+blarg : Int
+blarg =
+    10
+
+frangle : String
+frangle =
+    "ten"
+
+blargargle : Int -> String -> Bool
+blargargle num text =
+    toString num == text
+
+toStringCanMakeNumberText : Bool
+toStringCanMakeNumberText =
+    blargargle blarg frangle
+
+"""
+
+
+simpleExposingAll : String
+simpleExposingAll =
+    """module Simple exposing (..)
 
 blarg : Int
 blarg =
