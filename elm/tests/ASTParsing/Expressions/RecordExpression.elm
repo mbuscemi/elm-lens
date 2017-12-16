f4c5c3ac1a35fd18811656f3ac9995975c991dee
Matthew Buscemi <matthew.buscemi@gmail.com>
2017-12-16 13:00:04 -0800

    added coverage for references in record fields

---
+++ elm/tests/ASTParsing/Expressions/RecordExpression.elm
@@ -0,0 +1,67 @@
+module ASTParsing.Expressions.RecordExpression exposing (canParseFromRecord)
+
+import Dict
+import ElmFile
+import Expect
+import Set
+import Test exposing (Test, describe, test)
+import Types.Expression
+import Types.Reference exposing (Reference)
+
+
+canParseFromRecord : Test
+canParseFromRecord =
+    describe "Reference in Record Expression" <|
+        let
+            elmFile =
+                ElmFile.fromString "RecordExpression.elm" elmFileText
+        in
+        [ test "has expected module name" <|
+            \_ ->
+                Expect.equal elmFile.moduleName [ "Simple" ]
+        , test "has expected imports" <|
+            \_ ->
+                Expect.equal elmFile.imports
+                    { direct = Dict.empty
+                    , aliases = Dict.empty
+                    }
+        , test "has expected top level expressions" <|
+            \_ ->
+                Expect.equal elmFile.topLevelExpressions
+                    { functions =
+                        Dict.empty
+                            |> Dict.insert "reference" (Types.Expression.standardExpression 2)
+                            |> Dict.insert "recordExpression" (Types.Expression.standardExpression 6)
+                    , types = Dict.empty
+                    , typeAliases = Dict.empty
+                    }
+        , test "has expected exposings" <|
+            \_ ->
+                Expect.equal elmFile.exposings
+                    { functions =
+                        Set.empty
+                            |> Set.insert "recordExpression"
+                    , types = Set.empty
+                    }
+        , test "has expected references" <|
+            \_ ->
+                Expect.equal elmFile.references
+                    { internal = [ Reference "reference" ]
+                    , external = Dict.empty
+                    }
+        ]
+
+
+elmFileText : String
+elmFileText =
+    """module Simple exposing (recordExpression)
+
+reference : Int
+reference =
+    5
+
+recordExpression : { field: Int }
+recordExpression =
+    { field = reference }
+
+"""
